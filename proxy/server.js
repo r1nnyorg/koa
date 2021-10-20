42639ecba348573339a35e98d20f74a00d543899
chaowen guo <chaowen.guo1@gmail.com>
2021-10-19 21:38:51 -0500

    Update server.js

--- proxy/server.js
+++ proxy/server.js
@@ -1,1 +1,6 @@
-
+import Koa from 'koa'
+import proxy from 'koa-http2-proxy'
+import https from 'https'
+const app = new Koa
+app.use(proxy({ target: 'http://www.example.org' })).use(proxy)
+https.createServer({key:fs.readFileSync('/encrypt/privkey.pem'), cert:fs.readFileSync('/encrypt/fullchain.pem')}, app.callback()).listen(443)
