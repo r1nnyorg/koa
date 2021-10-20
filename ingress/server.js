1c494bdcc7916498267d36e519460c4d8d5ef3a3
chaowen guo <chaowen.guo1@gmail.com>
2021-10-19 22:36:24 -0500

    Update server.js

--- ingress/server.js
+++ ingress/server.js
@@ -5,5 +5,5 @@ import {promises as fs} from 'fs'
 
 const app = new Koa
 app.use(proxy('/web', {target:'http://web', pathRewrite:(path, req) => path.replace('/web', '')}))
-app.use(proxy('/chat', {target:'http://chat', pathRewrite:(path, req) => path.replace('/chat', '')}))
+app.use(proxy('/chat', {target:'http://chat', pathRewrite:(path, req) => path.replace('/chat', ''), ws:true}))
 https.createServer({key:await fs.readFile('/encrypt/privkey.pem'), cert:await fs.readFile('/encrypt/fullchain.pem')}, app.callback()).listen(443)
