dbed24d9dea646de077039fe223e4ea694b6786a
chaowen guo <chaowen.guo1@gmail.com>
2021-10-19 21:56:08 -0500

    Update server.js

--- proxy/server.js
+++ proxy/server.js
@@ -1,6 +1,9 @@
 import Koa from 'koa'
 import proxy from 'koa-http2-proxy'
 import https from 'https'
+import {promises as fs} from 'fs'
+
 const app = new Koa
-app.use(proxy({ target: 'http://www.example.org' })).use(proxy)
-https.createServer({key:fs.readFileSync('/encrypt/privkey.pem'), cert:fs.readFileSync('/encrypt/fullchain.pem')}, app.callback()).listen(443)
+app.use(proxy('/web', {target:'http://web', pathRewrite:{'/web/ajax':'/ajax'}}))
+app.use(proxy('/chat', {target:'http://chat', pathRewrite:{'/chat/ws':'/ws'}}))
+https.createServer({key:await fs.readFile('/encrypt/privkey.pem'), cert:await fs.readFile('/encrypt/fullchain.pem')}, app.callback()).listen(443)
