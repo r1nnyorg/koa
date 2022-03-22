11783f700910355c4e57ee11f7d216d92b5692c7
chaowen guo <chaowen.guo1@gmail.com>
2022-03-22 07:21:40 -0500

    Update server.js

--- web/server.js
+++ web/server.js
@@ -8,7 +8,7 @@ import {promises as fs} from 'fs'
 
 const app = new Koa
 //app.context.database = new pg.Pool({user:'postgres', database:'default', password:'pos1gres+', host:'postgrespostgres.postgres.database.azure.com', ssl:true})
-app.context.database = new pg.Pool({user:'root', database:'defaultdb', host:'cockroach', port:26257, ssl:{ca:await fs.readFile('ca.crt'), key:await fs.readFile('client.root.key'), cert:await fs.readFile('client.root.crt')}}) 
+app.context.database = new pg.Pool({user:'root', database:'defaultdb', host:'cockroach', port:26257, ssl:{rejectUnauthorized:false, ca:await fs.readFile('ca.crt'), key:await fs.readFile('client.root.key'), cert:await fs.readFile('client.root.crt')}}) 
 app.context.cache = new redis.Cluster([{host:'redis'}], {redisOptions:{password:process.env.password}}) //new redis({host:'redis'})  //new redis({host:'redis', family:6})
 app.use(body()).use(cors()).use(async ctx =>
 {
