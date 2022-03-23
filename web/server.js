e84248b6810987fcfa7bec68b568a023258fd0a4
chaowen guo <chaowen.guo1@gmail.com>
2022-03-23 03:39:11 -0500

    Update server.js

--- web/server.js
+++ web/server.js
@@ -5,10 +5,11 @@ import process from 'process'
 import pg from 'pg'
 import redis from 'ioredis'
 import {promises as fs} from 'fs'
+import path from 'path'
 
 const app = new Koa
 //app.context.database = new pg.Pool({user:'postgres', database:'default', password:'pos1gres+', host:'postgrespostgres.postgres.database.azure.com', ssl:true})
-app.context.database = new pg.Pool({user:'root', database:'defaultdb', host:'cockroach', port:26257, ssl:{rejectUnauthorized:false, ca:await fs.readFile('ca.crt'), key:await fs.readFile('client.root.key'), cert:await fs.readFile('client.root.crt')}}) 
+app.context.database = new pg.Pool({user:'root', database:'defaultdb', host:'cockroach', port:26257, ssl:{rejectUnauthorized:false, ca:await fs.readFile(path.join(path.dirname(new globalThis.URL(import.meta.url).pathname, 'ca.crt')), 'utf8'), key:await fs.readFile('client.root.key'), cert:await fs.readFile('client.root.crt')}}) 
 app.context.cache = new redis.Cluster([{host:'redis'}], {redisOptions:{password:process.env.password}}) //new redis({host:'redis'})  //new redis({host:'redis', family:6})
 app.use(body()).use(cors()).use(async ctx =>
 {
