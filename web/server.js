a04484fd13bc78efff5303e379f3a57ac019ce49
r1nny <109706512+r1nny@users.noreply.github.com>
2022-07-25 02:05:42 -0500

    Update server.js

--- web/server.js
+++ web/server.js
@@ -11,18 +11,15 @@ const app = new Koa
 //app.context.database = new pg.Pool({user:'postgres', database:'default', password:'pos1gres+', host:'postgrespostgres.postgres.database.azure.com', ssl:true})
 app.context.database = new pg.Pool({user:'root', database:'defaultdb', host:'cockroach', port:26257, ssl:{rejectUnauthorized:false, ca:await fs.readFile(path.join(path.dirname(new globalThis.URL(import.meta.url).pathname), 'ca.crt'), 'utf8'), key:await fs.readFile(path.join(path.dirname(new globalThis.URL(import.meta.url).pathname), 'client.root.key'), 'utf8'), cert:await fs.readFile(path.join(path.dirname(new globalThis.URL(import.meta.url).pathname), 'client.root.crt'), 'utf8')}}) 
 app.context.cache = new redis.Cluster([{host:'redis'}], {redisOptions:{password:process.env.password}}) //new redis({host:'redis'})  //new redis({host:'redis', family:6})
-console.log(process.env.password)
 app.use(body()).use(cors()).use(async ctx =>
 {
     if (globalThis.Object.is(ctx.request.path, '/ajax'))
     {
         const body = globalThis.Object.entries(globalThis.JSON.parse(ctx.request.body)).map(_ => _.join(' ')).join(' ')
         let records = await ctx.cache.get(body)
-        console.log(records)
         if (!records)
         {
             records = globalThis.JSON.stringify((await ctx.database.query(`select * from${body}`)).rows)
-            console.log(records)
             ctx.cache.set(body, records)
         }
         ctx.response.body = records
